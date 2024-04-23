9ca699f39f01daefca16f854cbeb32137ea124d6
Sebastian Christensen Mondrup <semon20@student.sdu.dk>
2024-04-23 16:12:22 +0200

    Create create_metadata.sh

---
+++ wildlifecam-scripts/create_metadata.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Param 1 = folder name
+# Param 2 = file name
+# Param 3 = trigger
+# Param 4 = created date
+
+CREATESECONDS=$(echo $4 | date +%s)
+TRIGGER=$3
+SUBJECTDISTANCE=$(exiftool -T -subjectdistance $1/$2)
+EXPOSURETIME=$(exiftool -T -exposuretime $1/$2)
+ISO=$(exiftool -T -ISO $1/$2)
+
+JSONFILENAME=$(echo $2 | sed 's/jpg/json/g')
+JSON="{\"File Name\": \"$2\", \"Create Date\": \"$4\", \"Create Seconds Epoch\": $CREATESECONDS, \"Trigg>
+echo $JSON
