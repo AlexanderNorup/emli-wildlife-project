d592bf09275123271ee9a7ae9e072b1c02517ee0
Sebastian Christensen Mondrup <semon20@student.sdu.dk>
2024-04-23 16:10:53 +0200

    Create timed_photo.sh

---
+++ wildlifecam-scripts/timed_photo.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+last_run=0
+
+while true; do
+        current_time=$(date +%s)
+        difference=$((current_time - last_run))
+        if [ "$difference" -ge 300 ]; then
+                last_run=$current_time
+                ./take_photo.sh
+        fi
+done
