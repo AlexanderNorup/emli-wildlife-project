12f0d10960a0e2775e2fb7a31c18bc2e2e7f4adc
Sebastian Christensen Mondrup <semon20@student.sdu.dk>
2024-04-30 12:10:32 +0200

    Update wipe_controller.sh

--- wildlifecam-scripts/wipe_controller.sh
+++ wildlifecam-scripts/wipe_controller.sh
@@ -9,18 +9,26 @@ DEVICE="/dev/ttyACM0"
 # Function to control wiper angle
 control_wiper() {
     angle="$1"
-    echo "{\"wiper_angle\": $angle}" > $DEVICE
+    if [ -c $DEVICE ]; then
+	echo "wiping"
+        echo "{\"wiper_angle\": $angle}" > $DEVICE
+    fi
+    echo "wiping potentially done"
 }
 
 while read msg
 do
     ./log_wildlife.sh "Beginning wipe of lens"
     control_wiper 180
-    sleep 1
+    echo "wipe 180"
+    sleep 2
     control_wiper 0
-    sleep 1
+    echo "wipe 0"
+    sleep 2
     control_wiper 180
-    sleep 1
+    echo "wipe 180"
+    sleep 2
     control_wiper 0
+    echo "wipe 0"
     ./log_wildlife.sh "Wiping of lens done"
 done < <(mosquitto_sub -h $MQTT_HOST -t $MQTT_TOPIC -u $MQTT_USER -P $MQTT_PASSWORD)
