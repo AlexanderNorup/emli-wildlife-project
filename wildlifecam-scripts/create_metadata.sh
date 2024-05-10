d276dd2c08dc85906fd25630d6b9d768248793bb
Sebastian Christensen Mondrup <semon20@student.sdu.dk>
2024-05-10 09:26:56 +0200

    Update create_metadata.sh

--- wildlifecam-scripts/create_metadata.sh
+++ wildlifecam-scripts/create_metadata.sh
@@ -11,6 +11,6 @@ SUBJECTDISTANCE=$(exiftool -T -subjectdistance $1/$2)
 EXPOSURETIME=$(exiftool -T -exposuretime $1/$2)
 ISO=$(exiftool -T -ISO $1/$2)
 
-JSONFILENAME=$(echo $2 | sed 's/jpg/json/g')
+JSONFILENAME=$(echo $2 | sed 's/jpg/json/g') # We can probably remove this
 JSON="{\"File Name\": \"$2\", \"Create Date\": \"$4\", \"Create Seconds Epoch\": $CREATESECONDS, \"Trigg>
 echo $JSON
